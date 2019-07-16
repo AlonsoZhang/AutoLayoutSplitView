b7d7d20278f3195e59ea20fc5a4cdb82f9fdc3e6
zhangwu <zhangwu@hyc.cn>
2019-07-16 19:31:09 +0800

    Demo

--- AutoLayoutSplitView/ViewController.swift
+++ AutoLayoutSplitView/ViewController.swift
@@ -10,11 +10,21 @@ import Cocoa
 
 class ViewController: NSViewController,NSSplitViewDelegate {
 
+    @IBOutlet weak var scrollView: NSScrollView!
     @IBOutlet weak var splitView: NSSplitView!
     @IBOutlet weak var leftPane: NSView!
     @IBOutlet weak var middlePane: NSView!
     @IBOutlet weak var rightPane: NSView!
+    @IBOutlet weak var slider: NSSlider!
     var lastLeftPaneWidth :CGFloat = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.leftPane.translatesAutoresizingMaskIntoConstraints = false
+        self.rightPane.translatesAutoresizingMaskIntoConstraints = false
+        lastLeftPaneWidth = self.leftPane.frame.size.width
+        splitView.delegate = self
+    }
     
     @IBAction func leftClick(_ sender: NSButton) {
 
@@ -46,12 +56,39 @@ class ViewController: NSViewController,NSSplitViewDelegate {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.leftPane.translatesAutoresizingMaskIntoConstraints = false
-        self.rightPane.translatesAutoresizingMaskIntoConstraints = false
-        lastLeftPaneWidth = self.leftPane.frame.size.width
-        splitView.delegate = self
+    @IBAction func LoadPicture(_ sender: NSButton) {
+        // load photo
+        let photoURL = Bundle.main.url(forResource: "butterfly", withExtension: "jpg")
+        var image:NSImage? = nil
+        if (photoURL != nil){
+            image = NSImage(contentsOf: photoURL!)
+        }
+        
+        //let originalSize = image?.size
+        let imageRect = NSRect(x: 0, y: 0, width: image?.size.width ?? 0.0, height: image?.size.height ?? 0.0)
+        let imageView = NSImageView(frame: imageRect)
+        imageView.bounds = imageRect
+        imageView.image = image
+        
+        // to pass to the bindings dictionary below - can't use dot notation (?)
+        //let scrollView = scrollView
+        scrollView.documentView = imageView
+        // turn off autoresizing for scroll view
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //let views = ["scrollView" : scrollView]
+        
+//        if let views = views as? [String : NSScrollView] {
+//            middlePane.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H|[scrollView]", options: [], metrics: nil, views: views))
+//            middlePane.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V|[scrollView]", options: [], metrics: nil, views: views))
+//        }
+    }
+    
+    @IBAction func slideAction(_ sender: NSSlider) {
+        let scaleFactor = slider.floatValue
+        //print(CGFloat(scaleFactor))
+        scrollView.magnification = CGFloat(scaleFactor)/100
+        scrollView.contentView.needsDisplay = true
     }
     
     func splitView(_ splitView: NSSplitView, canCollapseSubview subview: NSView) -> Bool {
@@ -62,12 +99,4 @@ class ViewController: NSViewController,NSSplitViewDelegate {
         }
         return false
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
